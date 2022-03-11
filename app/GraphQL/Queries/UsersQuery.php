9df2f251c093d20399b2af292ad69decc1295605
shaev <shaev@ugol.me>
2022-03-11 15:53:05 +0300

    - Реализовал разовтаризацию пользователя

--- app/GraphQL/Queries/UsersQuery.php
+++ app/GraphQL/Queries/UsersQuery.php
@@ -4,10 +4,15 @@ namespace App\GraphQL\Queries;
 use App\Models\User;
 use Closure;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Bus;
 use Rebing\GraphQL\Support\Facades\GraphQL;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Query;
+use Ticket\User\Application\User\GetUserCommand;
+use Ticket\User\Domain\UserAggregate;
+use Ticket\User\Domain\UserLocatorData;
 
 class UsersQuery extends Query
 {
@@ -17,35 +22,16 @@ class UsersQuery extends Query
 
     public function type(): Type
     {
-        return Type::listOf(GraphQL::type('user'));
-    }
-
-    public function args(): array
-    {
-        return [
-            'id' => [
-                'name' => 'id',
-                'type' => Type::string(),
-                'description' => 'UUid пользователя',
-            ],
-            'email' => [
-                'name' => 'email',
-                'type' => Type::string(),
-                'description' => 'email пользователя',
-            ]
-        ];
+        return GraphQL::type('user');
     }
 
     public function resolve($root, $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
     {
-        if (isset($args['id'])) {
-            return User::where('id' , $args['id'])->get();
-        }
+        /** @var UserAggregate $userEntity */
+        $userEntity = Bus::dispatchNow(
+            new GetUserCommand(UserLocatorData::fromStateAuth(Auth::guard()))
+        );
 
-        if (isset($args['email'])) {
-            return User::where('email', $args['email'])->get();
-        }
-
-        return User::all();
+        return $userEntity->toArray();
     }
 }
